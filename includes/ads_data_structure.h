045416626bb1f58bc77e061fcae4964e3b73ee29
BelaBelphegor <tiboitel@student.42.fr>
2017-02-04 18:46:49 +0100

    Adding commentaries. Uncomment to wrap with C++.

--- includes/ads_data_structure.h
+++ includes/ads_data_structure.h
@@ -6,11 +6,24 @@
 /*   By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 16:56:14 by tiboitel          #+#    #+#             */
-/*   Updated: 2017/02/04 16:56:32 by tiboitel         ###   ########.fr       */
+/*   Updated: 2017/02/04 18:45:33 by tiboitel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ADS_DATA_STRUCTURE_H
 # define FT_ADS_DATA_STRUCTURE_H
+
+/*
+** #ifdef __cplusplus
+** extern "C"
+** {
+** #endif
+*/
+
+/*
+** #ifdef __cplusplus
+** }
+** #endif
+*/
 
 #endif
