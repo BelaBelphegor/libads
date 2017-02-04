8e0ea66858fd0ab48de9601e79b2640e5d789c90
BelaBelphegor <tiboitel@student.42.fr>
2017-02-04 16:58:51 +0100

    First push. Implementation of the basic architecture of ADS

---
+++ Makefile
@@ -0,0 +1,42 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2014/11/03 16:28:46 by tiboitel          #+#    #+#              #
+#    Updated: 2017/02/04 16:50:51 by tiboitel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME		=	libads.a
+INCLUDES	=	./includes
+SRCS		=	ads_rand.c
+SRCSPATH	=	./srcs/
+CC 			=	clang
+CCFLAGS		=	-Wall -Werror -Wextra -O3
+INCLUDES_O	=	-I $(INCLUDES)
+INCLUDES_C	=	$(CFLAGS)
+SRC			= 	$(addprefix $(SRCSPATH), $(SRCS))
+OBJS		=	$(SRC:.c=.o)
+
+all:		$(NAME)
+
+$(NAME): $(OBJS)
+	ar rc $(NAME) $(OBJS)
+	ranlib $(NAME)
+
+%.o: %.c
+	echo $(SRCS)
+	$(CC) $(CCFLAGS) -c $< -o $@ $(INCLUDES_O)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: 	clean
+	rm -f $(NAME)
+
+re: 		fclean all
+
+.PHONY: all clean fclean re
---
+++ srcs/ads_rand.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ads_rand.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/04 16:45:06 by tiboitel          #+#    #+#             */
+/*   Updated: 2017/02/04 16:53:27 by tiboitel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ads_math.h"
+
+void	ads_random_generator_set_seed(uint32_t seed)
+{
+	srand(seed);
+}
+
+void	ads_random_generator_reset_seed(void)
+{
+	uint32_t	seed;
+
+	seed = getpid();
+	seed = ((seed >> 16) ^ seed) * 0x42D9F3B;
+	seed = ((seed >> 16) ^ seed) * 0x42D9F3B;
+	seed = ((seed >> 16) ^ seed);
+	srand(seed * time(NULL));
+}
+
+int32_t	ads_rand(void)
+{
+	return (rand());
+}
+
+int32_t	ads_rand_int_max(int max)
+{
+	return (rand() % (max + 1));
+}
+
+int32_t	ads_rand_int_interval(int min, int max)
+{
+	return (rand() % (max - min + 1) - min);
+}
+
+float	ads_rand_float_max(float max)
+{
+	return (0.0f + (rand() / (RAND_MAX / max)));
+}
+
+float	ads_rand_float_interval(float min, float max)
+{
+	return (min + (rand() / (RAND_MAX / (max - min))));
+}
+
+double	ads_rand_double_max(double max)
+{
+	return (0.0 + (rand() / (RAND_MAX / max)));
+}
+
+double	ads_rand_double_interval(double min, double max)
+{
+	return (min + (rand() / (RAND_MAX / (max - min))));
+}
